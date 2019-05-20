e5a634330b79aff6e7722cdc5d3aa0a13ef79328
danfrenette <dan.r.frenette@gmail.com>
2019-05-19 22:38:02 -0400

    Add layout scaffolding
    This was mainly adding layout files and components to the app. Layouts
    in this context being kinda railsy, but basically just meaning things
    that show up on every page. This was done by overwriting a folder next
    js uses called _app.js, and wrapping the `Component` prop it's passed
    with a `Page` component that I wrote. An interesting Meta component was
    also written to encapsulate the logic used for meta tags, like encodings
    and whatnot.

--- sick-fits/frontend/pages/index.jsx
+++ sick-fits/frontend/pages/index.jsx
@@ -1,9 +1,6 @@
-import Link from "next/link";
-
 const Home = props => (
   <div>
     <p>Hola!</p>
-    <Link href="/sell"><a>Sell!</a></Link>
   </div>
 )
 
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,9 +1,6 @@
-import Link from "next/link";
-
 const Sell = props => (
   <div>
     <p>sell supreme</p>
-    <Link href="/"><a>Home!</a></Link>
   </div>
 )
 
